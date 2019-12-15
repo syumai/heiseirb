bed22245459a96a4a001d3f68ec4fc63ac1c07a2
syumai <syumai@gmail.com>
2019-12-16 02:14:12 +0900

    Add implementation of my Enumerable

--- 2019/rubykaigi/A/template/my_enumerable.rb
+++ 2019/rubykaigi/A/template/my_enumerable.rb
@@ -1,20 +1,84 @@
 module Enumerable
   # eachを独自実装しよう！！！
   # forとかwhileでやってみてね。
-  def my_each; end
+  def my_each
+    for item in self
+      yield item
+    end
+  end
+
+  def my_each_with_index
+    for i in 0...self.count
+      yield self[i], i
+    end
+  end
 
   # 下記問題、eachの使用は禁止。my_eachとかreduceはいいよん
-  def my_find(arg); end
+  def my_find(arg=nil)
+    for item in self
+      return item if yield item
+    end
 
-  def my_map(arg); end
+    if arg
+      arg.call
+    else
+      nil
+    end
+  end
 
-  def my_each_with_object(arg); end
+  def my_map
+    result = []
+    my_each do |item|
+      result.push(yield item)
+    end
+    result
+  end
 
-  def my_group_by(arg); end
+  def my_each_with_object(arg)
+    for item in self
+      yield item, arg
+    end
+    arg
+  end
 
-  def tally; end
+  def my_group_by
+    result = {}
+    my_each do |item|
+      (result[yield item] ||= []).push(item)
+    end
+    result
+  end
+
+  def tally
+    result = {}
+    my_each do |item|
+      result[item] ||= 0
+      result[item] += 1
+    end
+    result
+  end
 end
 
 class Array
-  def my_zip(*args); end
+  def my_zip(*args, &block)
+    result = []
+    my_each_with_index do |item, i|
+      items = [item]
+      for ary in args
+        items.push(ary[i])
+      end
+
+      if block
+        block.call(items)
+      else
+        result[i] = items
+      end
+    end
+
+    if block
+      nil
+    else
+      result
+    end
+  end
 end
--- 2019/rubykaigi/A/template/my_enumerable_test.rb
+++ 2019/rubykaigi/A/template/my_enumerable_test.rb
@@ -13,7 +13,6 @@ class MyEnumerableTest < MiniTest::Test
 
   # https://ref.xaio.jp/ruby/classes/enumerable/find
   def test_find
-    skip
     assert_equal (1..100).find { |i| i % 5 == 0 && i % 7 == 0 }, (1..100).my_find { |i| i % 5 == 0 && i % 7 == 0 }
     not_found = -> { 'NOT FOUND' }
     assert_equal (1..100).find(not_found) { |i| i % 5 == 0 && i % 7 == 0 }, (1..100).my_find(not_found) { |i| i % 5 == 0 && i % 7 == 0 }
@@ -22,14 +21,12 @@ class MyEnumerableTest < MiniTest::Test
 
   # https://ref.xaio.jp/ruby/classes/enumerable/collect
   def test_map
-    skip
     assert_equal [1, 2, 3].map { |i| i ** 2 }, [1, 2, 3].my_map { |i| i ** 2 }
     assert_equal [1, 2, 3].map(&:to_s), [1, 2, 3].my_map(&:to_s)
   end
 
   # https://ref.xaio.jp/ruby/classes/array/zip
   def test_zip
-    skip
     arr1 = [1, 2, 3]
     arr2 = [4, 5]
     arr3 = [6, 7, 8, 9]
@@ -48,21 +45,18 @@ class MyEnumerableTest < MiniTest::Test
 
   # https://ref.xaio.jp/ruby/classes/enumerable/each_with_object
   def test_each_with_object
-    skip
     assert_equal (1..5).each_with_object([]) { |i, a| a << i * 2 }, (1..5).my_each_with_object([]) { |i, a| a << i * 2 }
     assert_equal %w(a li ce).each_with_object('Hello, ') { |str, memo| memo << str.upcase }, %w(a li ce).my_each_with_object('Hello, ') { |str, memo| memo << str.upcase }
   end
 
   # https://ref.xaio.jp/ruby/classes/enumerable/group_by
   def test_group_by
-    skip
     assert_equal([0, 1, 2].group_by(&:itself), [0, 1, 2].my_group_by(&:itself))
     assert_equal((1..6).group_by { |i| i % 3 }, (1..6).my_group_by { |i| i % 3 })
   end
 
   # https://medium.com/@baweaver/ruby-2-7-enumerable-tally-a706a5fb11ea
   def test_tally
-    skip
     assert_equal({ 1 => 3, 10 => 2, 100 => 4}, [1, 10, 100, 100, 10, 1, 100, 1, 100].tally)
   end
 end
